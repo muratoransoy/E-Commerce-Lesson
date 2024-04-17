2f204db2564f26dc593883e51b86c35e7294bf2e
murat.oransoy <murat.oransoy@301-00>
2024-04-17 19:23:08 +0300

    Add project files.

---
+++ E-Commerce.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
