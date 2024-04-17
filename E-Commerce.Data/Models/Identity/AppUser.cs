ba69bba904ae1cea18fe8a5df93abdc624c86cec
murat.oransoy <murat.oransoy@301-00>
2024-04-17 20:05:58 +0300

    Adding Nuget Package (SqlServer,Tools,Identity) Entities were created, database connection was made and migraiton was stared

---
+++ E-Commerce.Data/Models/Identity/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Models.Identity
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public AppUser(): base()
+        {
+
+        }
+        public AppUser(string userName) : base(userName)
+        {
+
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
